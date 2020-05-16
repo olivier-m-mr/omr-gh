54cd9ee42c9428c6ba7d09150fb41eccf8bc5032
Olivier Marie-Rose <olivier-m-mr@prometheuscomputing.com>
2020-05-16 08:54:43 -0400

    Updates

---
+++ precache-manifest.7acbd887d78bcd13acde0bd505daf700.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a86165350c071ab3c738bd6546f360",
+    "url": "/omr-gh/index.html"
+  },
+  {
+    "revision": "992c3dc873c158da1890",
+    "url": "/omr-gh/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5c39051751acff5b36b",
+    "url": "/omr-gh/static/js/2.3ee159c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/omr-gh/static/js/2.3ee159c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992c3dc873c158da1890",
+    "url": "/omr-gh/static/js/main.f570ac27.chunk.js"
+  },
+  {
+    "revision": "99fbfacc4c2ee8ccfde6",
+    "url": "/omr-gh/static/js/runtime-main.01de5e48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/omr-gh/static/media/logo.5d5d9eef.svg"
+  }
+]);
