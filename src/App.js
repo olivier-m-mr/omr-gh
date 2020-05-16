529dcdc999c38fbf2fddf09926db19331a2a5c7b
Olivier Marie-Rose <olivier-m-mr@prometheuscomputing.com>
2020-05-16 09:22:05 -0400

    added tree

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tree from './Tree';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div style={{ height: 400 }}>
+        <Tree />
+      </div>
     </div>
   );
 }
